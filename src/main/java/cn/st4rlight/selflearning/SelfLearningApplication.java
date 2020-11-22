f635c0b2e30d0c78cf0852f63295d92665289927
st4rlight <st4rlight@163.com>
2020-11-22 23:18:28 +0800

    Initial commit

---
+++ src/main/java/cn/st4rlight/selflearning/SelfLearningApplication.java
@@ -0,0 +1,13 @@
+package cn.st4rlight.selflearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelfLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelfLearningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cn/st4rlight/selflearning/SelfLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.st4rlight.selflearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelfLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
